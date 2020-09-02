a59d12dd8ebc1ca976f7a6ac3d13c4b5c18a14f1
rishavbhowmik <53208265+rishavbhowmik@users.noreply.github.com>
2020-09-02 13:10:02 +0530

    Update setdb.js

--- P2P/setdb.js
+++ P2P/setdb.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'werwerwer',
+  password : '',
   database : 'Peers',
   supportBigNumbers: true,
   bigNumberStrings: false,
